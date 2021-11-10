ec15ec9d81959e9bfa1225d35c3111388023aa3c
Russell Hunter <rhunter@indeed.com>
2021-11-10 14:31:37 -0600

    Added some testing  cases and text.

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,24 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/click count/i);
-  expect(linkElement).toBeInTheDocument();
 
-  screen.debug();
+  // To see the DOM that React Testing Library looks at:
+  // screen.debug();
+
+  // I generally use getByText().
+  // const linkElement = screen.getByText(/click count/i);
+  // expect(linkElement).toBeInTheDocument();
+
+  // But the docs say you should first use getByRole().
+  // To get a list of suggested roles to query by:
+  // screen.getByRole('');
+
+  // It can't find header, but it is in the document.
+  // You have to use the roles as defined in 
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles
+  // expect(screen.getByRole('header')).toBeInTheDocument();
+
+  // You have to use the role of banner.
+  // expect(screen.getByRole('banner')).toBeInTheDocument();
+
 });
