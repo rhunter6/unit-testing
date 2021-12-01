91b615bffb5d500a49da4babfb717ddea1adc377
Russell Hunter <rhunter@indeed.com>
2021-12-01 14:54:57 -0600

    Add comments in the code.

--- src/GetData/GetData.jsx
+++ src/GetData/GetData.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 
 export const GetData = () => {
+  // Set up some state hooks.
   const [loading, setLoading] = useState(true);
   const [remoteData, setRemoteData] = useState("");
+
+  // When the component loads, set a timeout to return data after 3 seconds.
+  // This is to simulate a slow network request.
   useEffect(() => {
     const simulatedFetch = async () => {
       const result = await new Promise((resolve) => {
@@ -15,9 +19,11 @@ export const GetData = () => {
     simulatedFetch();
   }, []);
 
+  // If we are loading, show loading text.
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  // Once we have data, show that.
   return <div>Your data is: {remoteData}</div>;
 };
