a6843297263a33c79cbc9c653f2af60e0b094eaa
Russell Hunter <rhunter@indeed.com>
2021-12-08 14:49:53 -0600

    Update unit test with solution.

--- src/GetData/GetData.test.js
+++ src/GetData/GetData.test.js
@@ -1,4 +1,5 @@
 import { render, screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { GetData } from "./GetData";
 
 describe("GetData testing", () => {
@@ -26,9 +27,31 @@ describe("GetData testing", () => {
 
     // Try #3 - Found out that I needed to wrap the update in a waitfor which wraps the code in an
     // act() call.  See https://reactjs.org/docs/testing-recipes.html#act
-    // waitFor(() => {
-    //     console.log('hello');
-    //   expect(screen.findByText("I have data.")).toBeInTheDocument();
+    //
+    // But this still gives me the "Warning: Can't perform a React state update on an unmounted component."
+    // await act(async () => {
+    //   await new Promise((resolve) => {
+    //     setTimeout(resolve, 4000);
+    //   });
     // });
+    // expect(screen.getByText("Your data is: I have data.")).toBeInTheDocument();
+    // screen.debug();
+  });
+
+  // Try #4.  Use jest fake timers!
+  it("should work with jest timeout", async () => {
+    // Let's me speed up setTimeout.
+    jest.useFakeTimers();
+
+    render(<GetData />);
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+    // Need to wrap this in an act so Rect updates correctly.
+    await act(async () => {
+      jest.advanceTimersByTime(3000);
+    });
+    expect(screen.getByText("Your data is: I have data.")).toBeInTheDocument();
+
+    // Go back to real timers.
+    jest.useRealTimers();
   });
 });
